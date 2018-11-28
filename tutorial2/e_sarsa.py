18875bc84a82b9c2472e2469fe14004eebfd619d
Lucas PAGE-CACCIA <lucas.page-caccia@mail.mcgill.ca>
2018-11-28 14:30:44 -0500

    fixed wayy to many bugs :'(

--- tutorial2/e_sarsa.py
+++ tutorial2/e_sarsa.py
@@ -6,10 +6,10 @@ np.set_printoptions(precision=4)
 # I'm moving away from the dictionary setup, as it's suboptimal for traces.
 
 class Sarsa:
-    def __init__(self, actions, world, epsilon=0.1, alpha=0.2, gamma=0.9, trace_coef=0.1, init_value=-1, algo='e_trace'):
+    def __init__(self, actions, world, epsilon=0.1, alpha=0.1, gamma=0.9, trace_coef=0.7, init_value=0, algo='e_trace'):
         self.q = np.zeros((world.width, world.height, len(actions))) + init_value
         self.trace = np.zeros((world.width, world.height, len(actions)))
-        
+
         print('using eligibility traces : {}'.format('trace' in algo))
         self.epsilon = epsilon
         self.alpha = alpha
@@ -18,6 +18,11 @@ class Sarsa:
         self.trace_coef = trace_coef # lambda or beta, depending
         self.algo = algo
         self.init_value = init_value
+        
+        # I keep two previous values. One for learning Q, and one for choosing actions
+        # they will be equivalent when performing online training, however will differ when evaluating
+        self.prev_value = None
+        self.prev_value_for_choosing_a = None
 
     def getQ(self, state, action):
         state = (state[0] - 1, state[1] - 1)
@@ -38,20 +43,33 @@ class Sarsa:
         if self.algo == 'e_trace':
             lambda_ = self.trace_coef
             self.trace = lambda_ * self.gamma * self.trace
-            self.trace[state, action] += 1
+            self.trace[state][action] += 1
         elif self.algo == 'h_trace':
             beta = self.trace_coef
             self.trace = (1 - beta) * self.gamma * self.trace
-            self.trace[state, action] += beta
+            self.trace[state][action] += beta
 
     def learnQ(self, state, action, reward, target):
         # SARSA(lambda) or SARSA(beta)
-        if 'trace' in self.algo:  
+        if self.algo == 'e_trace':
             self.update_trace(state, action)
-            oldv = self.getQ(state, action)
-            delta = target - oldv
-            self.setQ(state, action, oldv + self.alpha * delta * self.getE(state, action))
-        
+            delta = target - self.getQ(state, action)
+            self.q += self.alpha * delta * self.trace
+
+        elif self.algo == 'h_trace':  
+            self.update_trace(state, action)
+            q = self.getQ(state, action)
+            if self.prev_value is None:
+                q_tilde = q 
+            else:
+                beta = self.trace_coef
+                q_tilde = (1 - beta) * self.prev_value + beta * q
+
+            delta = target - q_tilde
+            self.q += self.alpha * delta * self.trace
+
+            self.prev_value = q_tilde
+
         # regular setup
         else: 
             oldv = self.getQ(state, action)
@@ -65,6 +83,11 @@ class Sarsa:
             action = random.choice(self.actions)
         else:
             q = [self.getQ(state, a) for a in self.actions]
+            if self.algo == 'h_trace':
+                if self.prev_value_for_choosing_a is not None:
+                    beta = self.trace_coef
+                    q = [(1 - beta) * self.prev_value_for_choosing_a + beta * q_sa for q_sa in q]
+
             maxQ = max(q)
             count = q.count(maxQ)
             if count > 1:
@@ -74,6 +97,10 @@ class Sarsa:
                 i = q.index(maxQ)
 
             action = self.actions[i]
+
+            # log for next iteration
+            self.prev_value_for_choosing_a = maxQ
+
         return action
 
     def learn(self, state1, action1, reward, state2, action2):
